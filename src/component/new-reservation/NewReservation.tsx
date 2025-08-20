8aaab0283b241a75a8e78259883d63da672a4ae7
Abhishek-Seek <abhishek0101811@gmail.com>
2025-08-20 10:08:33 +0530

    done

--- src/component/new-reservation/NewReservation.tsx
+++ src/component/new-reservation/NewReservation.tsx
@@ -3,9 +3,9 @@ import { Form, Input, Select, DatePicker, Button, Row, Col } from "antd";
 import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
-import React, { useState } from 'react';
-import { Checkbox, Divider, Table } from 'antd';
-import type { CheckboxOptionType, TableColumnsType } from 'antd';
+import React, { useState } from "react";
+import { Checkbox, Divider, Table } from "antd";
+import type { CheckboxOptionType, TableColumnsType } from "antd";
 
 const { Option } = Select;
 
@@ -17,37 +17,32 @@ interface DataType {
 }
 
 const columns: TableColumnsType<DataType> = [
-  { title: 'Column 1', dataIndex: 'address', key: '1' },
-  { title: 'Column 2', dataIndex: 'address', key: '2' },
-  { title: 'Column 3', dataIndex: 'address', key: '3' },
-  { title: 'Column 4', dataIndex: 'address', key: '4' },
-  { title: 'Column 5', dataIndex: 'address', key: '5' },
-  { title: 'Column 6', dataIndex: 'address', key: '6' },
-  { title: 'Column 7', dataIndex: 'address', key: '7' },
-  { title: 'Column 8', dataIndex: 'address', key: '8' },
+  { title: "Column 1", dataIndex: "address", key: "1" },
+  { title: "Column 2", dataIndex: "address", key: "2" },
+  { title: "Column 3", dataIndex: "address", key: "3" },
+  { title: "Column 4", dataIndex: "address", key: "4" },
+  { title: "Column 5", dataIndex: "address", key: "5" },
+  { title: "Column 6", dataIndex: "address", key: "6" },
+  { title: "Column 7", dataIndex: "address", key: "7" },
+  { title: "Column 8", dataIndex: "address", key: "8" },
 ];
 
 const data: DataType[] = [
   {
-    key: '1',
-    name: 'John Brown',
+    key: "1",
+    name: "John Brown",
     age: 32,
-    address: 'New York Park',
+    address: "New York Park",
   },
   {
-    key: '2',
-    name: 'Jim Green',
+    key: "2",
+    name: "Jim Green",
     age: 40,
-    address: 'London Park',
+    address: "London Park",
   },
 ];
 
 const defaultCheckedList = columns.map((item) => item.key);
-
-
-
-
-
 
 export default function NewReservation() {
   const [form] = Form.useForm();
